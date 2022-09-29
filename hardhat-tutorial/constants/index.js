0f26faaafecd560fcbaff0468388b3c7306e2008
pupirkin <pupirkin02@gmail.com>
2022-09-29 19:02:53 +0600

    new

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x60FDe2540b66c23acffF1FE5C22db6DAaC35DFC6";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://as-s29poinmt-my-app-ajhr.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
